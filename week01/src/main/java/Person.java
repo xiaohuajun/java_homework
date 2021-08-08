93a4e08cc10e3cb4b565201f74c806f9ffae4270
xiaohuajun <18061741650@163.com>
2021-08-08 14:51:18 +0800

    测试类

--- week01/src/main/java/Person.java
+++ week01/src/main/java/Person.java
@@ -1,5 +1,8 @@
 /**
  * @author xiaohuajun
+ * @version 1.0
+ * @date 2021/8/8 下午2:51
+ * @description Person
  */
 public class Person {
 
---
+++ week01/src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author xiaohuajun
+ * @version 1.0
+ * @date 2021/8/8 下午2:50
+ * @description Test
+ */
+public class Test {
+
+}
