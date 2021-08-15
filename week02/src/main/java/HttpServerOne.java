197e6e9fcdedd83862de26d986b608743f33f12f
xiaohuajun <18061741650@163.com>
2021-08-15 22:13:17 +0800

    第二周作业

---
+++ week02/src/main/java/HttpServerOne.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author xiaohuajun
+ * @version 1.0
+ * @date 2021/8/15 下午9:31
+ * @description HttpServerOne : socket 模拟http服务
+ */
+public class HttpServerOne {
+
+
+  public static void main(String[] args) throws IOException{
+    ServerSocket serverSocket = new ServerSocket(8801);
+    while (true) {
+      try {
+        Socket socket = serverSocket.accept();
+        server(socket);
+      } catch (IOException e) {
+       throw  new RuntimeException(e.getMessage());
+      }
+    }
+  }
+
+
+  public static void server(Socket socket){
+    try {
+      PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+      printWriter.println("HTTP/1.1 200 OK");
+      printWriter.println("Content-Type:text/html;charset=utf-8");
+      String body = "hello,http";
+      printWriter.println("Content-Length:" + body.getBytes().length);
+      printWriter.println();
+      printWriter.write(body);
+      printWriter.close();
+      socket.close();
+    } catch (IOException e) {
+      throw new RuntimeException("网络异常",e);
+    }
+  }
+
+}
---
+++ week02/src/main/java/http/HttpClientRequest.java
@@ -0,0 +1,43 @@
+package http;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author xiaohuajun
+ * @version 1.0
+ * @date 2021/8/15 下午9:49
+ * @description HttpClientRequest : 发起http请求
+ */
+public class HttpClientRequest {
+
+
+
+
+  public static void main(String[] args) {
+    String url = "http://localhost:8801";
+    System.out.println(httpRequest(url));
+  }
+
+
+  public static String httpRequest(String url) {
+    // 创建 HttpClient 客户端
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    // 创建 HttpGet 请求
+    HttpGet httpGet = new HttpGet(url);
+    try (CloseableHttpResponse httpResponse = httpClient.execute(httpGet)) {
+      System.out.println(httpResponse.getStatusLine());
+      HttpEntity entity1 = httpResponse.getEntity();
+      return EntityUtils.toString(entity1, "UTF-8");
+    } catch (IOException e) {
+      throw new RuntimeException("调用异常：", e);
+    }
+  }
+
+
+}
